a8c92269829e10240ddf213c0e284835da27def9
Diego Felix <diegoflx.oliveira@gmail.com>
2011-06-08 23:45:08 -0500

    initial commit of laravel!

---
+++ app/database/migrations/2014_02_13_203140_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name', 100);
+			$table->string('username', 30)->unique();
+			$table->string('email', 80)->unique();
+			$table->string('password', 64);
+			$table->tinyInteger('gender');
+			$table->integer('points')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
