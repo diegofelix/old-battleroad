4fc2dd3ac95fc11e25ad1d076cf50cda79868e74
Diego Felix <diegoflx.oliveira@gmail.com>
2014-02-19 17:50:03 -0300

    Starting test the Google OAuth

--- app/routes.php
+++ app/routes.php
@@ -16,21 +16,21 @@ Route::get('/', function()
 	// get data from input
     $code = Input::get( 'code' );
 
-    // get fb service
-    $google = OAuth::consumer( 'Google' );
+    // get google service
+    $googleService = OAuth::consumer( 'Google' );
 
     // check if code is valid
 
     // if code is provided get user data and sign in
     if ( !empty( $code ) ) {
 
-        // This was a callback request from facebook, get the token
-        $token = $google->requestAccessToken( $code );
+        // This was a callback request from google, get the token
+        $token = $googleService->requestAccessToken( $code );
 
         // Send a request with it
-        $result = json_decode( $google->request( '/me' ), true );
+        $result = json_decode( $googleService->request( 'https://www.googleapis.com/oauth2/v1/userinfo' ), true );
 
-        $message = 'Your unique facebook user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
+        $message = 'Your unique Google user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
         echo $message. "<br/>";
 
         //Var_dump
@@ -40,11 +40,10 @@ Route::get('/', function()
     }
     // if not ask for permission first
     else {
-        // get fb authorization
-        $url = $google->getAuthorizationUri();
+        // get googleService authorization
+        $url = $googleService->getAuthorizationUri();
 
         // return to facebook login url
         return Redirect::to((string)$url);
-        return Response::make()->header( 'Location', (string)$url );
     }
 });
