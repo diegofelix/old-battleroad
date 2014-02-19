7913763a36ae54b1288d2c4ca63edcab840c13f7
Diego Felix <diegoflx.oliveira@gmail.com>
2014-02-19 18:22:21 -0300

    changed the google credentials

--- app/config/packages/artdarek/oauth-4-laravel/config.php
+++ app/config/packages/artdarek/oauth-4-laravel/config.php
@@ -22,8 +22,8 @@ return array(
 		 * Google
 		 */
         'Google' => array(
-            'client_id'     => '839794533312.apps.googleusercontent.com',
-            'client_secret' => 'rWb7vUkKrloWS96s3OEoRrOb',
+            'client_id'     => '839794533312-c46nn3icqe7iov792jmuqtde8qtqj84t.apps.googleusercontent.com',
+            'client_secret' => 'Oj8FzAAa6t_PUETPko9EYOBp',
             'scope'         => array('userinfo_email', 'userinfo_profile'),
         ),
 
