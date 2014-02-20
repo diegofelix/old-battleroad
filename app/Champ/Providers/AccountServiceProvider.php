aba57676f5b5deb499d8bcbc01e1cf89eedae7eb
Diego Felix <diegoflx.oliveira@gmail.com>
2014-02-20 18:16:31 -0300

    fixed the providers problem

---
+++ app/Champ/Providers/AccountServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Champ\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AccountServiceProvider extends ServiceProvider {
+
+  /**
+   * Register the binding
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->app->bind('Champ\Account\UserRepositoryInterface', 'Champ\Account\UserRepository');
+  }
+
+}
