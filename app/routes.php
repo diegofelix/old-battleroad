15d4ab699644646fa9deae5269c0fd245610b338
Diego Felix <diegoflx.oliveira@gmail.com>
2014-02-21 17:18:17 -0300

    Added a test in the routes file

--- app/routes.php
+++ app/routes.php
@@ -6,3 +6,12 @@ Route::get('/', function()
 {
     return View::make('hello');
 });
+
+Route::get('/diego', function()
+{
+    $user = App::make('Champ\Account\UserRepositoryInterface');
+
+    $userFound = $user->all();
+
+    dd($userFound);
+});
