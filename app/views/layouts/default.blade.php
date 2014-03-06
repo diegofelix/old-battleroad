cff8de4b52a244ffb8c0acb836e9cf8c5e5dd3a8
Diego Felix <diegoflx.oliveira@gmail.com>
2014-03-06 10:30:06 -0300

    Start coding the front-end

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="O primeiro site de campeonatos do brasil!">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Champaholic - De olho nos campeões</title>
+
+        <link href="//fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <div id="wrapper">
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                            <span class="sr-only">Menu</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+
+                        <a href="/" class="navbar-brand">
+                            Champaholic
+                        </a>
+                    </div> <!-- navbar-header -->
+
+                    <div class="collapse navbar-collapse navbar-ex1-collapse">
+
+                        <ul class="nav navbar-nav">
+                            <li><a href="#">Jogos</a></li>
+                            <li><a href="#">Campeonatos</a></li>
+                            <li><a href="#">Feedback</a></li>
+                        </ul>
+
+                        <ul class="nav navbar-nav navbar-right">
+                        </ul>
+                    </div> <!-- collapse -->
+                </div> <!-- container -->
+            </nav>
+        </div>
+    </body>
+</html>
