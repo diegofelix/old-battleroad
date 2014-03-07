2d97fd935dfbc5963e492d6452606714d25f918b
Diego Felix <diegoflx.oliveira@gmail.com>
2014-03-06 23:34:07 -0300

    Changed the home and added more content to layout

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -16,6 +16,7 @@
     <body>
 
         <div id="wrapper">
+
             <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
                 <div class="container">
                     <div class="navbar-header">
@@ -25,22 +26,37 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a href="/" class="navbar-brand">Champaholic</a>
                     </div> <!-- navbar-header -->
 
                     <div class="collapse navbar-collapse navbar-ex1-collapse">
 
                         <ul class="nav navbar-nav">
-                            <li><a href="#">Jogos</a></li>
-                            <li><a href="#">Campeonatos</a></li>
+                            <li><a href="#">Como Funciona?</a></li>
                             <li><a href="#">Feedback</a></li>
+                            <li><a href="#">Ajuda</a></li>
                         </ul>
 
                         <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Login</a></li>
                         </ul>
                     </div> <!-- collapse -->
                 </div> <!-- container -->
             </nav>
-        </div>
+
+            <div class="piece banner">
+                
+                <div id="call-subscribe">
+                    <header>
+                        <h1><a href="#">Champaholic</a></h1>
+                    </header>
+                    <section>
+                        <p>A primeira plataforma de campeonatos do Brasil. Mostre quem é o melhor!</p>
+                        <p><a href="#" title="Cadastre-se" class="btn button-subscribe">Cadastre-se!</a></p>
+                    </section>
+                </div>
+
+            </div>
+
+        </div><!-- wrapper -->
     </body>
 </html>
