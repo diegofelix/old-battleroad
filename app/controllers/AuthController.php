4882876894e325e9b394dd8c22661d50f0008aa5
Diego Felix <diegoflx.oliveira@gmail.com>
2014-03-15 13:41:55 -0300

    Refactoring the oAuth Service for Google

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,13 +1,8 @@
 <?php
 
-use Champ\Social\SocialAuthenticatorListener;
+use Champ\Social\SocialAuthenticatorListenerInterface;
 
-class AuthController extends BaseController implements SocialAuthenticatorListener {
-
-    public function __construct()
-    {
-
-    }
+class AuthController extends BaseController implements SocialAuthenticatorListenerInterface {
 
     /**
      * Handle the Authentication from Google
@@ -17,7 +12,7 @@ class AuthController extends BaseController implements SocialAuthenticatorListen
     public function google()
     {
         if (Input::has('code')) {
-            return App::make('Champ\Social\Google\GoogleAuthenticator')->authByCode($this, Input::get('code'));
+            return App::make('Champ\Social\Google\GoogleAuthenticator')->auth($this, Input::get('code'));
         }
 
         // redirect to the google oAuth
