a1adc9de7a92fb4ec7c95042da15150c3399d45a
Diego Felix <diegoflx.oliveira@gmail.com>
2014-04-11 23:21:35 -0300

    Added more design to manage championship

--- app/assets/js/championship.js
+++ app/assets/js/championship.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
+    // mask the inputs when creating a championship
     $("#price").inputmask('[9][9]99,99', { numericInput: true, "placeholder" : "" });
     $("#event_start, #event_end").inputmask('d/m/y h:s:00');
 
+    $('.description-label').hide();
+
+    // show and hide divs when clicked
+    $('.show-hide').click(function(){
+        $this = $(this);
+        $this.find('.info').slideToggle();
+        $this.find('.description-label').toggle();
+    });
 });
