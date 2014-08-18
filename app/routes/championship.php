fdcbeeae6646e781393d87fdaf6106c287a4015c
Diego Felix <diegoflx.oliveira@gmail.com>
2014-08-18 17:35:32 -0300

    more tests with checkout

--- app/routes/championship.php
+++ app/routes/championship.php
@@ -4,4 +4,6 @@
 Route::resource('championships', 'ChampionshipsController', ['only' => ['index', 'show']]);
 
 Route::get('payment/{id}', ['as' => 'payment', 'uses' => 'ChampionshipsController@payment']);
-Route::post('moip', 'ChampionshipsController@moipReturn');
+Route::any('moip', function(){
+    echo 'ok';
+});
