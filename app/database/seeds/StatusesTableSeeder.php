e8755cccf72f8e210f4caff552534ab467a9f2f0
Diego Felix <diegoflx.oliveira@gmail.com>
2014-08-29 17:03:56 -0300

    changed the statuses messages

--- app/database/seeds/StatusesTableSeeder.php
+++ app/database/seeds/StatusesTableSeeder.php
@@ -6,50 +6,56 @@ class StatusesTableSeeder extends Seeder {
     {
         DB::table('statuses')->insert([
             'id' => 1,
-            'name' => 'Autorizado',
-            'description' => 'Pagamento autorizado pelo pagador, porém ainda não creditado para o recebedor em razão do floating'
+            'name' => 'Aguardando pagamento',
+            'description' => 'Estamos aguardando o pagamento.'
         ]);
 
         DB::table('statuses')->insert([
             'id' => 2,
-            'name' => 'Iniciado',
-            'description' => 'Pagamento foi iniciado, mas não existem garantias de que será finalizado'
+            'name' => 'Em análise',
+            'description' => 'Estamos analisando seu pagamento, a qualquer momento seu pagamento será confirmado.'
         ]);
 
         DB::table('statuses')->insert([
             'id' => 3,
-            'name' => 'BoletoImpresso',
-            'description' => 'Pagamento ainda não foi confirmado, porém boleto bancário foi impresso e pode ter sido pago (não existem garantias de que será pago)'
+            'name' => 'Pago',
+            'description' => 'Já recebemos seu pagamento, agora você poderá participar do campeonato!'
         ]);
 
         DB::table('statuses')->insert([
             'id' => 4,
-            'name' => 'Concluido',
-            'description' => 'Pagamento foi concluído, dinheiro debitado do pagador e creditado para o recebedor'
+            'name' => 'Disponível',
+            'description' => 'O prazo para disputa chegou ao fim, significa que não poderá receber seu dinheiro devolta.'
         ]);
 
         DB::table('statuses')->insert([
             'id' => 5,
-            'name' => 'Cancelado',
-            'description' => 'Pagamento foi cancelado por quem estava pagando'
+            'name' => 'Em disputa',
+            'description' => 'Uma pena que você esteja cancelando, esperamos que participe de outros campeonatos com a gente!'
         ]);
 
         DB::table('statuses')->insert([
             'id' => 6,
-            'name' => 'EmAnalise',
-            'description' => 'Pagamento autorizado pelo pagador, mas está em análise e não tem garantias de que será autorizado'
+            'name' => 'Dinheiro devolvido',
+            'description' => 'Já devolvemos seu dinheiro.'
         ]);
 
         DB::table('statuses')->insert([
             'id' => 7,
-            'name' => 'Estornado',
-            'description' => 'Pagamento foi concluído, dinheiro creditado para o recebedor, porém estornado para o cartão de crédito do pagador'
+            'name' => 'Cancelado',
+            'description' => 'Ocorreu algum erro ou através de sua solicitação a transação foi cancelada.'
         ]);
 
         DB::table('statuses')->insert([
             'id' => 9,
-            'name' => 'Reembolsado',
-            'description' => 'Pagamento foi concluído, dinheiro creditado para o recebedor, porém houve o reembolso para a Carteira Moip do pagador'
+            'name' => 'Chargeback debitado',
+            'description' => 'Já devolvemos seu dinheiro.'
+        ]);
+
+        DB::table('statuses')->insert([
+            'id' => 9,
+            'name' => 'Em contestação',
+            'description' => 'Aparentemente você pediu o estorno junto ao seu cartão de crédito, estamos verificando.'
         ]);
     }
 
