af2fbc97350044d9c6829599178c62c55edd50c2
Diego Felix <diegoflx.oliveira@gmail.com>
2014-09-01 18:57:22 -0300

    added dashboard to user menu and inverted the position of the championships

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -14,6 +14,32 @@
 
     <div class="container">
 
+        <div class="panel panel-primary">
+            <div class="panel-heading">Campeonatos que estou participando</div>
+            <div class="panel-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Título</th>
+                            <th>Organizador</th>
+                            <th>Status</th>
+                            <th>Detalhes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($joins as $join)
+                            <tr>
+                                <td>{{ $join->championship->name }}</td>
+                                <td>{{ $join->user->name }}</td>
+                                <td>{{ $join->status->name }}</td>
+                                <td><a href="{{ route('join.show', $join->id) }}"><span class="icon icon-eye"></span> Detalhes</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
         <div class="panel panel-warning">
             <div class="panel-heading">Meus Campeonatos</div>
             <div class="panel-body">
@@ -33,32 +59,6 @@
                                 <td>{{ ($championship->published) ? 'Publicado' : 'Não publicado' }}</td>
                                 <td>{{ $championship->joins->count() }}</td>
                                 <td><a href="{{ route('admin.championships.show', $championship->id) }}"><span class="icon icon-cog"></span> Gerenciar</a></td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
-        <div class="panel panel-info">
-            <div class="panel-heading">Campeonatos que estou participando</div>
-            <div class="panel-body">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Título</th>
-                            <th>Organizador</th>
-                            <th>Status</th>
-                            <th>Detalhes</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($joins as $join)
-                            <tr>
-                                <td>{{ $join->championship->name }}</td>
-                                <td>{{ $join->user->name }}</td>
-                                <td>{{ $join->status->name }}</td>
-                                <td><a href="{{ route('join.show', $join->id) }}"><span class="icon icon-eye"></span> Detalhes</a></td>
                             </tr>
                         @endforeach
                     </tbody>
