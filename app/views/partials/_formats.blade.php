49039b8dc2e2289bd0af453c7f19aa4b4542dd3b
Diego Felix <diegoflx.oliveira@gmail.com>
2015-01-22 15:13:59 -0200

    added tooltip in show championship and typo fixes

---
+++ app/views/partials/_formats.blade.php
@@ -0,0 +1,29 @@
+<!-- Modal -->
+<div class="modal fade" id="formats" tabindex="-1" role="dialog" aria-labelledby="formatsLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="formatsLabel">Formatos</h4>
+      </div>
+      <div class="modal-body">
+        <p>Existem 4 principais tipos de campeonatos:</p>
+
+        <dl>
+            <dt>Single Elimination</dt>
+            <dd>É o famosos mata-mata, quem perder é eliminado.</dd>
+            <dt>Double Elimination</dt>
+            <dd>Parecido com mata-mata, porém os jogadores que perderem a primeira vez vão para um série a parte, chamada de losers ( perdedores ). Lá ele terá mais uma chance.</dd>
+            <dt>Round Robin</dt>
+            <dd>Conhecido aqui no Brasil como pontos corridos onde todos enfrentam todos, pode haver variantes.</dd>
+            <dt>Swiss</dt>
+            <dd>Esse é um meio termo entre Double Elimination e Round Robin.</dd>
+        </dl>
+
+        <p>Para mais informações, por favor acesse: <a href="http://battleroad.uservoice.com/knowledgebase/articles/419839-quais-os-tipos-de-campeonato"> Tipos de campeonato.</a></p>
+
+      </div>
+    </div>
+  </div>
+</div>
+<!-- // Modal -->
