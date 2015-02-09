4c77077439ee7a67d7cef0d246652fd233ba5ebf
Diego Felix <diegoflx.oliveira@gmail.com>
2015-02-09 11:42:35 -0200

    starting coding coupons

---
+++ app/database/migrations/2015_02_03_181712_create_coupons_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCouponsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('coupons', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('code', 11)->unique();
+			$table->unsignedInteger('championship_id');
+			$table->unsignedInteger('user_id')->nullable();
+			$table->boolean('used')->default(false);
+			$table->timestamps();
+			$table->foreign('championship_id')->references('id')->on('championships');
+			$table->foreign('user_id')->references('id')->on('users');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('coupons');
+	}
+
+}
