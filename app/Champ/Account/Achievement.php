52382f60b13112c0f161c887963c9ac4b5a06d31
Diego Felix <diegoflx.oliveira@gmail.com>
2015-03-16 13:22:40 -0300

    added the achievements funcionality

---
+++ app/Champ/Account/Achievement.php
@@ -0,0 +1,44 @@
+<?php namespace Champ\Account;
+
+use Eloquent;
+
+class Achievement extends Eloquent {
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = ['championship', 'competition.game'];
+
+    /**
+     * Relation with User
+     *
+     * @return BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('Champ\Account\User');
+    }
+
+    /**
+     * Relation with Championship
+     *
+     * @return BelongsTo
+     */
+    public function championship()
+    {
+        return $this->belongsTo('Champ\Championship\Championship');
+    }
+
+    /**
+     * Relation with Competition
+     *
+     * @return BelongsTo
+     */
+    public function competition()
+    {
+        return $this->belongsTo('Champ\Championship\Competition');
+    }
+
+}
