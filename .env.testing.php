a5b364e89a85dc6ba3ff3a26ffc86c044f2f4a19
Diego Felix <diegoflx.oliveira@gmail.com>
2015-03-18 17:23:14 -0300

    added testes

---
+++ .env.testing.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'APP_ENV' => 'local',
+    // Database settings
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'champaholic',
+    'DB_USER' => 'root',
+    'DB_PASS' => 'root',
+
+     // MOIP settings
+    'MOIP_ENV'      => 'test',
+    'MOIP_KEY'      => 'ABABABABABABABABABABABABABABABABABABABAB',
+    'MOIP_TOKEN'    => '01010101010101010101010101010101',
+
+    // PAGSEGURO settings
+    'PAGSEGURO_EMAIL' => 'diegoflx.oliveira@gmail.com',
+    'PAGSEGURO_TOKEN' =>  'B36A4ADF9F7E4B58A82E26D2D8AA4BBD',
+    //'PAGSEGURO_TOKEN' =>  'E613A83C26C344B4AB6A4CAADE19DCC0',
+    //
+    // Original
+    // 'MERCADOLIVRE_CLIENT' => '327781618675455',
+    // 'MERCADOLIVRE_SECRET' => 'nTpwgusnAxp7P3bjmDGlVHqbxcOXVI7x',
+
+    // Teste
+    'MERCADOLIVRE_CLIENT' => '3348313329700561',
+    'MERCADOLIVRE_SECRET' => '8qRy6R72rul2XKPnh2Wz18AsC2r7Atya',
+
+    // BCash Account
+    'BCASH_ACCOUNT' => 'diegoflx.oliveira@gmail.com',
+    'BCASH_TOKEN'   => '909E12A17C8B38FF13545BF1639A8615',
+
+    // mandril settings
+    'MANDRILL_SECRET' => '3zD6rbq59FcpQmuMUm_ZXA',
+
+    // Mailchimp settings
+    'MAILCHIMP_APIKEY' => '1e0a9b7ea570193a15452a331e4988cb-us7',
+    'CHAMPIONSHIPS_SUBSCRIBERS_LIST' => '4d9ee789f6',
+
+    'CHALLONGE_API' => 'WdYqANmBpQTe7mWzFomEArHsaxqfWPblJBTsuZnJ'
+];
