70ae98a78a8a62ad1e0fc8dd176a702a8d7432d7
Diego Felix <diegoflx.oliveira@gmail.com>
2015-04-08 16:25:08 -0300

    Improve ( i think ) the layout in some sections of the platform

--- app/views/admin/championships/coupons/index.blade.php
+++ app/views/admin/championships/coupons/index.blade.php
@@ -6,7 +6,7 @@
 
     {{ Form::open(['route' => ['admin.championships.coupons.generate', $championship->id], 'role' => 'form', 'class' => 'form-inline']) }}
 
-        <div class="form-group col-md-2">
+        <div class="form-group">
             <label class="sr-only" for="percentage">Porcentagem</label>
             <div class="input-group ">
                 <div class="input-group-addon">R$</div>
@@ -19,26 +19,28 @@
 
     <hr>
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Código</th>
-                <th>Usuário</th>
-                <th>Valor</th>
-                <th>Data de Criação</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($championship->coupons as $coupon)
-                <tr @if ($coupon->user) class="success" @endif>
-                    <td><input type="text" class="form-control" value="{{ $coupon->code }}" readonly></td>
-                    <td>@if($coupon->user) {{ $coupon->user->name }} @endif</td>
-                    <td>{{ $coupon->present()->userPrice }}</td>
-                    <td>{{ $coupon->created_at->format('d/m/Y') }}</td>
+    <div class="panel panel-default">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Usuário</th>
+                    <th>Valor</th>
+                    <th>Data de Criação</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($championship->coupons as $coupon)
+                    <tr @if ($coupon->user) class="success" @endif>
+                        <td><input type="text" class="form-control" value="{{ $coupon->code }}" readonly></td>
+                        <td>@if($coupon->user) {{ $coupon->user->name }} @endif</td>
+                        <td>{{ $coupon->present()->userPrice }}</td>
+                        <td>{{ $coupon->created_at->format('d/m/Y') }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @stop
 @section('scripts')
     {{ HTML::script('js/register.js') }}
