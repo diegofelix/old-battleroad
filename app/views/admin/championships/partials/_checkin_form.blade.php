4892f5b86da395a8456024c8df223d82eb78026c
Diego Felix <diegoflx.oliveira@gmail.com>
2015-04-13 12:48:28 -0400

    fixed a bug in the checkin

--- app/views/admin/championships/partials/_checkin_form.blade.php
+++ app/views/admin/championships/partials/_checkin_form.blade.php
@@ -1,6 +1,6 @@
 {{ Form::open(['route' => ['checkin', $join->id], 'class' => 'checkin-form']) }}
 
-    <button type="submit" class="checkin-button btn btn-default @if ($join->checkin) bnt-success @endif" data-loading-text="Salvando.." autocomplete="off">
+    <button type="submit" class="checkin-button btn @if ($join->checkin) btn-success @else btn-default @endif" data-loading-text="Salvando.." autocomplete="off">
         <i class="fa fa-check"></i> Check In
     </button>
 
