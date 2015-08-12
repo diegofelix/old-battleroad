fda9f49891ffaec7dfd0ee5d1188c43220eb27aa
Diego Felix <diegoflx.oliveira@gmail.com>
2015-08-12 09:30:10 -0300

    championship list more organized in dashboard

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -13,7 +13,7 @@
                      Registrar Novo Campeonato
                 </a>
             </h2>
-        
+
             <div class="panel panel-warning">
                 <div class="panel-heading">Meus Campeonatos</div>
                 <div class="panel-body">
@@ -26,7 +26,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($championships as $championship)
+                            @foreach ($activeChampionships as $championship)
                                 <tr>
                                     <td>{{ $championship->name }}</td>
                                     <td>
@@ -43,6 +43,35 @@
                         </tbody>
                     </table>
                 </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Campeonatos passados</div>
+                <table class="champ-table">
+                    <thead>
+                        <tr>
+                            <th>Título</th>
+                            <th>Status</th>
+                            <th>Participantes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($oldChampionships as $championship)
+                            <tr>
+                                <td>{{ $championship->name }}</td>
+                                <td>
+                                    @if ($championship->published)
+                                        <span class="fa fa-toggle-on"></span>
+                                    @else
+                                        <span class="fa fa-toggle-off"></span>
+                                    @endif
+                                </td>
+                                <td>{{ $championship->joins->count() }}</td>
+                                <td><a href="{{ route('admin.championships.show', $championship->id) }}"><span class="fa fa-cog"></span></a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
