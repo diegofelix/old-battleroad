473828af6f2bc820d07f68d96db5a63d7107aee3
Diego Felix <diegoflx.oliveira@gmail.com>
2017-09-05 21:48:51 -0300

    fix error on property

--- app/Champ/Forms/RegisterForm.php
+++ app/Champ/Forms/RegisterForm.php
@@ -5,7 +5,7 @@ use Laracasts\Validation\FormValidator;
 
 class RegisterForm extends FormValidator
 {
-    protected $rules => [
+    protected $rules = [
         'name' => 'required|min:4',
         'username' => 'required|between:5,20|unique:users|alpha_dash',
         'email' => 'required|email|unique:users',
