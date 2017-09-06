91f90ec26a0f159453342bd8d1cd5976d9d50a17
Diego Felix <diegoflx.oliveira@gmail.com>
2017-09-05 21:47:49 -0300

    more style fixes

--- app/Champ/Forms/CompetitionForm.php
+++ app/Champ/Forms/CompetitionForm.php
@@ -1,15 +1,15 @@
-<?php namespace Champ\Forms;
+<?php
+namespace Champ\Forms;
 
 use Laracasts\Validation\FormValidator;
 
-class CompetitionForm extends FormValidator {
-
+class CompetitionForm extends FormValidator
+{
     protected $rules => [
         'game_id' => 'required',
         'platform_id' => 'required',
         'format' => 'required',
-        'price' => 'sometimes|numeric'
-        'event_start' => 'required|date_format:"d/m/Y"|future_date'
+        'price' => 'sometimes|numeric',
+        'event_start' => 'required|date_format:"d/m/Y"|future_date',
     ];
-
 }
--- app/Champ/Forms/RegisterForm.php
+++ app/Champ/Forms/RegisterForm.php
@@ -1,14 +1,14 @@
-<?php namespace Champ\Forms;
+<?php
+namespace Champ\Forms;
 
 use Laracasts\Validation\FormValidator;
 
-class RegisterForm extends FormValidator {
-
+class RegisterForm extends FormValidator
+{
     protected $rules => [
         'name' => 'required|min:4',
         'username' => 'required|between:5,20|unique:users|alpha_dash',
         'email' => 'required|email|unique:users',
-        'password' => 'required|confirmed'
+        'password' => 'required|confirmed',
     ];
-
 }
