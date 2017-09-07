a1d3b3f416805f6379b531f64917fa97717b7c89
Diego Felix <diegoflx.oliveira@gmail.com>
2017-09-07 18:24:02 -0300

    fix links on admin sidebar

--- resources/views/partials/_admin_sidebar.blade.php
+++ resources/views/partials/_admin_sidebar.blade.php
@@ -11,12 +11,12 @@
             @endif
 
             <ul>
-                {{ champ_action_links('Informações', '', 'admin.championships.show', $championship->id, 'fa fa-info-circle'); }}
-                {{ champ_action_links('Banner', 'banner', 'admin.championships.banner', $championship->id, 'fa fa-camera'); }}
-                {{ champ_action_links('Jogos', 'games', 'admin.championships.games', $championship->id, 'fa fa-gamepad'); }}
-                {{ champ_action_links('Participantes', 'users', 'admin.championships.users', $championship->id, 'fa fa-users'); }}
-                {{ champ_action_links('Cupons', 'coupons', 'admin.championships.coupons', $championship->id, 'fa fa-ticket'); }}
-                {{ champ_action_links('Correio', 'mail', 'admin.championships.mail', $championship->id, 'fa fa-envelope'); }}
+                {!! champ_action_links('Informações', '', 'admin.championships.show', $championship->id, 'fa fa-info-circle'); !!}
+                {!! champ_action_links('Banner', 'banner', 'admin.championships.banner', $championship->id, 'fa fa-camera'); !!}
+                {!! champ_action_links('Jogos', 'games', 'admin.championships.games', $championship->id, 'fa fa-gamepad'); !!}
+                {!! champ_action_links('Participantes', 'users', 'admin.championships.users', $championship->id, 'fa fa-users'); !!}
+                {!! champ_action_links('Cupons', 'coupons', 'admin.championships.coupons', $championship->id, 'fa fa-ticket'); !!}
+                {!! champ_action_links('Correio', 'mail', 'admin.championships.mail', $championship->id, 'fa fa-envelope'); !!}
                 {{-- champ_action_links('Feedback', 'feedback', 'admin.championships.feedback', $championship->id, 'icon-star'); --}}
             </ul>
 
