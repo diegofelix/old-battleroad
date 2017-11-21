38f3366d1a1eca773416a7ab309445a2ba02fc42
Diego Felix <diegoflx.oliveira@gmail.com>
2017-11-21 14:22:38 -0200

    delete embedded championship funcionality

--- app/Http/routes/championship.php
+++ app/Http/routes/championship.php
@@ -2,5 +2,3 @@
 
 // Championships
 Route::resource('championships', 'ChampionshipsController', ['only' => ['index', 'show']]);
-Route::get('championships/{championships}/embeded', ['as' => 'championships.embeded', 'uses' => 'EmbededJoinController@index']);
-Route::post('championships/{championships}/embeded', ['as' => 'championships.embeded', 'uses' => 'EmbededJoinController@store']);
