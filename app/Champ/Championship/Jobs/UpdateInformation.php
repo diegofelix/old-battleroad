e3d49eb4c38d40f319d8fa642f6c3feb291ea838
Diego Felix <diegoflx.oliveira@gmail.com>
2017-12-10 22:29:03 -0200

    finished championship update information job

--- app/Champ/Championship/Jobs/UpdateInformation.php
+++ app/Champ/Championship/Jobs/UpdateInformation.php
@@ -1,7 +1,54 @@
 <?php
-
 namespace Champ\Championship\Jobs;
 
-class UpdateInformation
+use Battleroad\Jobs\Job;
+use Champ\Championship\Repository;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateInformation extends Job implements SelfHandling
 {
+    use SerializesModels;
+
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @var string
+     */
+    public $stream;
+
+    public function __construct($id, $name, $description, $stream = '')
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->stream = $stream;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Repository $repository)
+    {
+        $championship = $repository->find($this->id);
+
+        $championship->updateInformation($this->name, $this->description, $this->stream);
+
+        $repository->save($championship);
+
+        return $championship;
+    }
 }
